1e507c4b0ff57989d8290a0f0a944114d5409e94
ivanjermakov <vania6600@gmail.com>
2019-09-19 14:28:21 +0300

    util classes

---
+++ src/main/java/com/gmail/ivanjermakov1/jtrue/util/Equals.java
@@ -0,0 +1,18 @@
+package com.gmail.ivanjermakov1.jtrue.util;
+
+import java.util.function.Predicate;
+
+public class Equals<T> implements Predicate<T> {
+
+	private final T to;
+
+	public Equals(T to) {
+		this.to = to;
+	}
+
+	@Override
+	public boolean test(T t) {
+		return t.equals(to);
+	}
+
+}
