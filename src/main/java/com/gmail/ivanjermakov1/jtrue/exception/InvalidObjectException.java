bdd54676b62d63fd53eb35c8e83c4422def6cbdc
ivanjermakov <vania6600@gmail.com>
2019-09-19 13:14:15 +0300

    bootstrap exception

---
+++ src/main/java/com/gmail/ivanjermakov1/jtrue/exception/InvalidObjectException.java
@@ -0,0 +1,12 @@
+package com.gmail.ivanjermakov1.jtrue.exception;
+
+public class InvalidObjectException extends RuntimeException {
+
+	public InvalidObjectException() {
+	}
+
+	public InvalidObjectException(String s) {
+		super(s);
+	}
+
+}
