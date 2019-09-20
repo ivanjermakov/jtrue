189a96fa327ae0475773cf86e6779c66423b0038
ivanjermakov <vania6600@gmail.com>
2019-09-20 08:49:03 +0300

    add tests

---
+++ src/test/java/com/gmail/ivanjermakov1/jtrue/unit/predicate/IsBlankUnitTest.java
@@ -0,0 +1,26 @@
+package com.gmail.ivanjermakov1.jtrue.unit.predicate;
+
+import com.gmail.ivanjermakov1.jtrue.predicate.IsBlank;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsBlankUnitTest {
+
+	@Test
+	public void shouldBeBlank() {
+		assertTrue(new IsBlank().test(""));
+	}
+
+	@Test
+	public void shouldNotBeBlank() {
+		assertFalse(new IsBlank().test("str"));
+	}
+
+	@Test
+	public void shouldNotBeBlankWithNull() {
+		assertFalse(new IsBlank().test(null));
+	}
+
+}
---
+++ src/test/java/com/gmail/ivanjermakov1/jtrue/unit/predicate/IsEmptyCollectionUnitTest.java
@@ -0,0 +1,28 @@
+package com.gmail.ivanjermakov1.jtrue.unit.predicate;
+
+import com.gmail.ivanjermakov1.jtrue.predicate.IsEmptyCollection;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsEmptyCollectionUnitTest {
+
+	@Test
+	public void shouldBeEmpty() {
+		assertTrue(new IsEmptyCollection<>().test(Collections.emptyList()));
+	}
+
+	@Test
+	public void shouldNotBeEmpty() {
+		assertFalse(new IsEmptyCollection<>().test(Collections.singletonList(new Object())));
+	}
+
+	@Test
+	public void shouldNotBeEmptyWithNull() {
+		assertFalse(new IsEmptyCollection<>().test(null));
+	}
+
+}
