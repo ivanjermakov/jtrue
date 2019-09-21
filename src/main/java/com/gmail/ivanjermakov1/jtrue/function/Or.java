7a0bb3e9798da3ec282905d45e9573747b8ee9e6
ivanjermakov <vania6600@gmail.com>
2019-09-21 19:13:41 +0300

    add functions

--- src/main/java/com/gmail/ivanjermakov1/jtrue/function/Or.java
+++ src/main/java/com/gmail/ivanjermakov1/jtrue/function/Or.java
@@ -5,13 +5,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.function.BinaryOperator;
 
-public class And implements BinaryOperator<Boolean> {
+public class Or implements BinaryOperator<Boolean> {
 
 	@Override
 	@NotNull
 	@Contract(pure = true)
 	public Boolean apply(@NotNull Boolean b1, @NotNull Boolean b2) {
-		return b1 && b2;
+		return b1 || b2;
 	}
 
 }
