54e74d317752b4eee080d6be404f43335fb31d09
ivanjermakov <vania6600@gmail.com>
2019-09-21 22:06:10 +0300

    add tests

---
+++ src/test/java/com/github/ivanjermakov/jtrue/unit/predicate/GreaterThanUnitTest.java
@@ -0,0 +1,21 @@
+package com.github.ivanjermakov.jtrue.unit.predicate;
+
+import com.github.ivanjermakov.jtrue.predicate.GreaterThan;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GreaterThanUnitTest {
+
+	@Test
+	public void shouldBeTrue() {
+		assertTrue(new GreaterThan<>(1).test(2));
+	}
+
+	@Test
+	public void shouldNotBeTrue() {
+		assertFalse(new GreaterThan<>(5).test(2));
+	}
+
+}
---
+++ src/test/java/com/github/ivanjermakov/jtrue/unit/predicate/LessThanUnitTest.java
@@ -0,0 +1,21 @@
+package com.github.ivanjermakov.jtrue.unit.predicate;
+
+import com.github.ivanjermakov.jtrue.predicate.LessThan;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LessThanUnitTest {
+
+	@Test
+	public void shouldBeTrue() {
+		assertTrue(new LessThan<>(2).test(1));
+	}
+
+	@Test
+	public void shouldNotBeTrue() {
+		assertFalse(new LessThan<>(2).test(5));
+	}
+
+}
